558795a694f7c6272f7626804ff309502df0396d
Volodymyr <klymenkodp@gmail.com>
2019-03-18 23:31:18 -0400

    Add webView

--- Project4/EasyBrowser/EasyBrowser/ViewController.swift
+++ Project4/EasyBrowser/EasyBrowser/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://react-cupertino.github.io/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
