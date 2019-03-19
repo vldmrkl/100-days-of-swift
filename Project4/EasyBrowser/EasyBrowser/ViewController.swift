04f0c59d18f988521ec628df46953fac102363a3
Volodymyr <klymenkodp@gmail.com>
2019-03-18 23:07:54 -0400

    Initial commit 🎉?

---
+++ Project4/EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Volodymyr Klymenko on 2019-03-18.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
