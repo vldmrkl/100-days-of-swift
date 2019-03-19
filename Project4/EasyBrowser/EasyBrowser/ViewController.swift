d3e93c711fb530d7442d0eb4f66455842e1f4174
Volodymyr <klymenkodp@gmail.com>
2019-03-18 23:47:30 -0400

    Add site selection

--- Project4/EasyBrowser/EasyBrowser/ViewController.swift
+++ Project4/EasyBrowser/EasyBrowser/ViewController.swift
@@ -20,11 +20,29 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         let url = URL(string: "https://react-cupertino.github.io/")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
     }
 
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "reactjs.org", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "react-cupertino.github.io", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
 
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
